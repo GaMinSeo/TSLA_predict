ed5bd617448c09c131f87e96a4643c34ad85640a
GaMinSeo <rkehfdl23@gmail.com>
2024-05-07 15:51:37 +0900

    출처 버튼 리메이크

--- home.py
+++ home.py
@@ -5,5 +5,5 @@ def home_run():
     st.subheader('이 앱은 테슬라 주가를 예측하는 앱 대시보드 입니다.')
     st.subheader('지난 주가를 확인하고 앞으로의 주가를 예측해봅시다.')
     st.image('./image/elon_musk1.jpg')
-    st.markdown('[데이터 출처](https://www.nasdaq.com/market-activity/stocks/tsla/historical)')
-    st.markdown('[이미지 출처](https://www.hankyung.com/article/2021051567297)')
+    st.link_button('데이터 출처',url = 'https://www.nasdaq.com/market-activity/stocks/tsla/historical')
+    st.link_button('이미지 출처',url = 'https://www.hankyung.com/article/2021051567297')
